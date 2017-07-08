ac8c0251d97e919c1c585b967de490b685d67406
Chetan Meshram <bash@Chetans-MacBook-Pro.local>
2017-07-08 16:38:39 -0700

    keys js

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'jQNkrU1sgpngurcNNzryGBpjS',
+  consumer_secret: 'ygmhUcLJn1wH4dhZoioIA6W2X86ZGRLXymKtQI0f6rch6IpNRn',
+  access_token_key: '517893382-sCnMFQVOCaOXEetJvFHBz8Yi7WJ2rNa8AV8rSFoA',
+  access_token_secret: 'u5JdZYLoWlEis5reHlumCAp1CyrCdIxEtIg8xPX6EYZnF',
+}
